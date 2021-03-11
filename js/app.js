03ec8333b6052653df575def820f85923b40a025
Molly <molly.liebeskind@effem.com>
2021-03-11 11:39:53 -0500

    added functionality to open and close mobile navigation with dynamically generated anchor points

--- js/app.js
+++ js/app.js
@@ -5,20 +5,24 @@ function generateNavigation() {
     i = 1;
 
     for (section of sections) {
-        if  (i != 1) {
+        // if  (i != 1) {
             const sectionName = section.getAttribute("data-nav");
             const sectionID = section.getAttribute("id");
-            const newElement = document.createElement("a");
-            newElement.textContent = sectionName;
-            newElement.className += 'nav-item';
-            newElement.href = `#${sectionID}`;
+            const newElement = document.createElement("li");
+            const linkElement = document.createElement("a")
+            linkElement.textContent = sectionName;
+            linkElement.classList.add("nav-item");
+            linkElement.href = `#${sectionID}`;
+            linkElement.style.background = "#89C9D0";
+            // newElement.style = "color: #22395c; font-weight: bolder"
 
+            newElement.appendChild(linkElement);
             tempNav.appendChild(newElement);
-        };
-        i += 1;
+        // };
+        // i += 1;
     };
 
-    navItem = document.getElementById("nav-container");
+    navItem = document.getElementById("nav-list");
 
     navItem.appendChild(tempNav);
 
@@ -66,8 +70,107 @@ document.addEventListener('scroll', function() {
 
         for (navItem of navItems) {
             navItem.style = "initial";
+            navItem.style.color = "#22395c"
         };
     
     };
 
 });
+
+function slideDown(target, duration) {
+    target.style.removeProperty('display');
+    const display = window.getComputedStyle(target).display;
+    
+    if (display === 'none') {
+        display = 'block';
+    }
+    target.style.display = display;
+
+    // ensure that the starting state is hidden
+    const height = target.offsetHeight;    
+    target.style.height = 0;    
+    target.style.paddingTop = 0; 
+    target.style.paddingBottom = 0;  
+    target.style.marginTop = 0;  
+    target.style.marginBottom = 0;  
+    target.style.overflow = 'hidden'; 
+
+    // slide down
+    target.style.boxSizing = 'border-box'; 
+    target.style.transitionProperty = "height, margin, padding";  
+    target.style.transitionDuration = duration + 'ms'; 
+    target.style.height = height + 'px'; 
+    target.style.removeProperty('padding-top');   
+    target.style.removeProperty('padding-bottom');   
+    target.style.removeProperty('margin-top');   
+    target.style.removeProperty('margin-bottom');  
+
+    // remove uneeded properties off screen
+    window.setTimeout(function(){
+        target.style.removeProperty('height'); 
+        target.style.removeProperty('overflow'); 
+        target.style.removeProperty('transition-duration'); 
+        target.style.removeProperty('transition-property'); 
+      }, duration);
+
+    }
+
+function slideUp(target, duration) {
+    // set the transition properties
+    target.style.transitionProperty = 'height, margin, padding'; 
+    target.style.transitionDuration = duration + 'ms'; 
+    target.style.boxSizing = 'border-box'; 
+    target.style.height = target.offsetHeight + 'px';
+
+    // hide the target
+    target.style.height = 0; 
+    target.style.paddingTop = 0; 
+    target.style.paddingBottom = 0; 
+    target.style.marginTop = 0; 
+    target.style.marginBottom = 0; 
+    target.style.overflow = 'hidden';
+
+    // after action completes, remove the rest of the properties off screen
+    window.setTimeout(function() {
+        target.style.display = 'none'; 
+        target.style.removeProperty('height');
+        target.style.removeProperty('padding-top');  
+        target.style.removeProperty('padding-bottom');  
+        target.style.removeProperty('margin-top');  
+        target.style.removeProperty('margin-bottom');  
+        target.style.removeProperty('overflow');         
+        target.style.removeProperty('transition-duration');  
+        target.style.removeProperty('transition-property');  
+      }, 
+      duration);
+    };
+
+function slideToggle(target, duration) {
+    let slideToggle = (target, duration = 500) => {
+        if (window.getComputedStyle(target).display === 'none') {
+          return slideDown(target, duration);
+        } else {
+          return slideUp(target, duration);
+        }
+      }
+} ;
+
+
+function navDrop() {
+    // const button = document.getElementById("nav-toggle");
+    const navList = document.querySelector("#nav-list")
+
+    if (navList.className == "nav-list-up") {
+        navList.style.display = "block";
+        navList.style.position = "absolute";
+        navList.style.zindex = "5000000000";
+        navList.classList.remove("nav-list-up");
+        navList.classList.add("nav-list-down");
+    }
+
+    else {
+        navList.style.display = "none";
+        navList.classList.remove("nav-list-down");
+        navList.classList.add("nav-list-up");
+    }
+};
